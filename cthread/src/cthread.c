160698eaaec370ec7105f12ae5d464d5ee08ddbc
vianna274 <leonardo.vianna.f@gmail.com>
2018-03-27 21:48:21 -0300

    cidentify implemented

---
+++ cthread/src/cthread.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <string.h>
+#include "../include/cthread.h"
+#include "../include/cdata.h"
+#include "../include/support.h"
+
+/******************************************************************************
+Par�metros:
+	name:	ponteiro para uma �rea de mem�ria onde deve ser escrito um string que cont�m os nomes dos componentes do grupo e seus n�meros de cart�o.
+		Deve ser uma linha por componente.
+	size:	quantidade m�xima de caracteres que podem ser copiados para o string de identifica��o dos componentes do grupo.
+Retorno:
+	Se correto => 0 (zero)
+	Se erro	   => Valor negativo.
+******************************************************************************/
+int cidentify (char *name, int size) {
+  int i = 0;
+	char identity[73] = {0};
+
+	strcpy(identity, "Leonardo 00274721\nAndy Garramones 00274XXX\nGuilherme Haetinger 00274XXX\n");
+	for(i=0;i<size && i < 73;i++) {
+		name[i] = identity[i];
+	}
+};
+
+/******************************************************************************
+Par�metros:
+	start:	ponteiro para a fun��o que a thread executar�.
+	arg:	um par�metro que pode ser passado para a thread na sua cria��o.
+	prio:	N�O utilizado neste semestre, deve ser sempre zero.
+Retorno:
+	Se correto => Valor positivo, que representa o identificador da thread criada
+	Se erro	   => Valor negativo.
+******************************************************************************/
+int ccreate (void* (*start)(void*), void *arg, int prio);
+
+/******************************************************************************
+Par�metros:
+	Sem par�metros
+Retorno:
+	Se correto => 0 (zero)
+	Se erro	   => Valor negativo.
+******************************************************************************/
+int cyield(void);
+
+/******************************************************************************
+Par�metros:
+	tid:	identificador da thread cujo t�rmino est� sendo aguardado.
+Retorno:
+	Se correto => 0 (zero)
+	Se erro	   => Valor negativo.
+******************************************************************************/
+int cjoin(int tid);
+
+/******************************************************************************
+Par�metros:
+	tid:	identificador da thread a ser suspensa.
+Retorno:
+	Se correto => 0 (zero)
+	Se erro	   => Valor negativo.
+******************************************************************************/
+int csuspend(int tid);
+
+/******************************************************************************
+Par�metros:
+	tid:	identificador da thread que ter� sua execu��o retomada.
+Retorno:
+	Se correto => 0 (zero)
+	Se erro	   => Valor negativo.
+******************************************************************************/
+int cresume(int tid);
+
+/******************************************************************************
+Par�metros:
+	sem:	ponteiro para uma vari�vel do tipo csem_t. Aponta para uma estrutura de dados que representa a vari�vel sem�foro.
+	count: valor a ser usado na inicializa��o do sem�foro. Representa a quantidade de recursos controlados pelo sem�foro.
+Retorno:
+	Se correto => 0 (zero)
+	Se erro	   => Valor negativo.
+******************************************************************************/
+int csem_init(csem_t *sem, int count);
+
+/******************************************************************************
+Par�metros:
+	sem:	ponteiro para uma vari�vel do tipo sem�foro.
+Retorno:
+	Se correto => 0 (zero)
+	Se erro	   => Valor negativo.
+******************************************************************************/
+int cwait(csem_t *sem);
+
+/******************************************************************************
+Par�metros:
+	sem:	ponteiro para uma vari�vel do tipo sem�foro.
+Retorno:
+	Se correto => 0 (zero)
+	Se erro	   => Valor negativo.
+******************************************************************************/
+int csignal(csem_t *sem);
