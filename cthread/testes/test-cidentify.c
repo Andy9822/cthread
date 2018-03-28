7c5fc3a9cac0e40826851b875b8d31bc0d571e50
vianna274 <leonardo.vianna.f@gmail.com>
2018-03-27 22:07:08 -0300

    Output improved

--- cthread/testes/test-cidentify.c
+++ cthread/testes/test-cidentify.c
@@ -11,11 +11,26 @@
 
 int main(int argc, char *argv[]) {
 
-	char nome[100] = {0};
+	char name[100] = {0}, name2[100] = {0};
+	int response = 027;
 
-	cidentify(nome, 99);
+	puts("Testing - cidentify");
 
-	puts(nome);
+	response = cidentify(name, 99);
+	if (response == 0) {
+		puts("First Case - SUCCEEDED");
+	} else {
+		puts("First Case - FAILED");
+	}
+
+	response = 027;
+
+	response = cidentify(name2, 50);
+	if (response == -1) {
+		puts("Second Case - SUCCEEDED");
+	} else {
+		puts("Second Case - FAILED");
+	}
 
 	return 0;
 }
