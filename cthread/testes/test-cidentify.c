09ea248fd7df33415cca5759cfc246b2f2dc5291
vianna274 <leonardo.vianna.f@gmail.com>
2018-04-01 17:00:44 -0300

    Using Logger

--- cthread/testes/test-cidentify.c
+++ cthread/testes/test-cidentify.c
@@ -1,35 +1,34 @@
-
 /*
- *	Programa de Testes
- *
- * Teste da funcao cidentify
+ *	Test Program - cidentify
  */
+
+#include <stdio.h>
 
 #include "../include/support.h"
 #include "../include/cthread.h"
-#include <stdio.h>
+#include "../include/LGA_logger.h"
 
 int main(int argc, char *argv[]) {
 
 	char name[100] = {0}, name2[100] = {0};
 	int response = 027;
 
-	puts("Testing - cidentify");
+	LGA_LOGGER_TEST("Testing cidentify");
 
 	response = cidentify(name, 99);
 	if (response == 0) {
-		puts("First Case - SUCCEEDED");
+		LGA_LOGGER_TEST("First Case - SUCCEEDED");
 	} else {
-		puts("First Case - FAILED");
+		LGA_LOGGER_TEST("First Case - FAILED");
 	}
 
 	response = 027;
 
 	response = cidentify(name2, 50);
 	if (response == -1) {
-		puts("Second Case - SUCCEEDED");
+		LGA_LOGGER_TEST("Second Case - SUCCEEDED");
 	} else {
-		puts("Second Case - FAILED");
+		LGA_LOGGER_TEST("Second Case - FAILED");
 	}
 
 	return 0;
