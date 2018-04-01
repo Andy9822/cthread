44a5af6dcffa3fd453b9f44319567044432c05e0
vianna274 <leonardo.vianna.f@gmail.com>
2018-04-01 17:01:11 -0300

    Test Ccreate updated and using logger

--- cthread/testes/test-ccreate.c
+++ cthread/testes/test-ccreate.c
@@ -1,37 +1,56 @@
 /*
- *	Programa de exemplo de uso da biblioteca cthread
- *
- *	Vers�o 1.0 - 14/04/2016
- *
- *	Sistemas Operacionais I - www.inf.ufrgs.br
- *
+ *	Test Program - ccreate
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+
 #include "../include/cthread.h"
+#include "../include/LGA_logger.h"
 
 void* func0(void *arg) {
-	printf("Eu sou a thread ID0 imprimindo %d\n", *((int *)arg));
+	if (*(int *)arg == 5) {
+		LGA_LOGGER_TEST("First Test SUCCEEDED");
+	} else {
+		LGA_LOGGER_TEST("First Test FAILED");
+	}
 	return 0;
 }
 
 void* func1(void *arg) {
-	printf("Eu sou a thread ID1 imprimindo %d\n", *((int *)arg));
+	if (*(int *)arg == 2) {
+		LGA_LOGGER_TEST("Second Test SUCCEEDED");
+	} else {
+		LGA_LOGGER_TEST("Second Test FAILED");
+	}
 	return 0;
 }
 
 int main(int argc, char *argv[]) {
 
 	int	id0, id1;
-	int i;
+	int i = 5;
+
+	LGA_LOGGER_TEST("Testing ccreate and cjoin");
+
+	LGA_LOGGER_LOG("Creating both threads");
 
 	id0 = ccreate(func0, (void *)&i, 0);
 	id1 = ccreate(func1, (void *)&i, 0);
 
-	printf("Eu sou a main ap�s a criacao de ID0 e ID1 %d\n", id0);
+	LGA_LOGGER_LOG("Main after creating both threads");
 
+	if(id0 == 0)
+		cjoin(1);
 
-	printf("Eu sou a main voltando para terminar o programa %d\n", id1);
+	LGA_LOGGER_LOG("Main after thread 1");
+	LGA_LOGGER_ERROR("CUUU");
+	i = 2;
+
+	if(id1 == 0)
+		cjoin(2);
+
+	LGA_LOGGER_LOG("Main after thread 2");
 
 	return 0;
 }
